2fa6d16fecefca96ffdf72e02e402cf915cb8496
preet-singh <preetsingh13@gmail.com>
2019-09-26 09:44:22 -0500

    updates

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ const { NODE_ENV } = require('./config');
 
 const app = express();
 const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
-
-app.use(morgan(morganOption));
+//NODE_ENV = will be production by default on heroku 
+app.use(morgan(morganOption));//want to use a different morgan depending on dev env
 app.use(cors());
 app.use(helmet());
 
